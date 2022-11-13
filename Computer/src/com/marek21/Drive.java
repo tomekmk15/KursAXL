e2872b9de9287d0404fbe2ed0a03eaf0e59f8e31
Tomasz Marek <tomekmk15@gmail.com>
2022-11-12 18:15:28 -0600

    modyficatiom Drive

--- Computer/src/com/marek21/Drive.java
+++ Computer/src/com/marek21/Drive.java
@@ -1,25 +1,4 @@
 package com.marek21;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class Drive {
-
-    List<File> hddFiles = new ArrayList<>();
-    Map<String, File> sdd = new HashMap<>();
-    private String type;
-
-    public Drive(String type){
-
-
-        this.type = type;
-    }
-    public void addFile(File file){
-        if(type.equals(("")))
-
-
-    }
-
+public interface Drive {
 }
